1a6833ffe6fc702fd8db589cf2630a08646e28aa
shaonsajid <shaonsajidul@gmail.com>
2022-01-31 23:25:16 +0600

    Update Deleet TestNG Allure

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,47 +1,48 @@
+
 import org.apache.commons.configuration.ConfigurationException;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class TestRunner {
     Customer customer;
 
-    @Test
+    @Test(priority = 0, description = "Log In")
     public void doLogin() throws ConfigurationException, IOException {
         customer = new Customer();
         customer.callingLoginAPI();
     }
 
-    @Test
+    @Test(priority = 1, description = "Get all customers")
     public void getCustomerList() throws ConfigurationException, IOException {
         customer = new Customer();
         customer.callingCustomerListAPI();
     }
 
-    @Test
+   @Test(priority = 2, description = "Search Customer")
     public void searchCustomer() throws ConfigurationException, IOException {
         customer = new Customer();
         customer.callingCustomerSearchAPI(102);
     }
 
-    @Test
+    @Test(priority = 3, description = "Generate Random Customer")
     public void generateCustomerInfo() throws ConfigurationException, IOException {
         customer = new Customer();
         customer.generateCustomer();
     }
-    @Test
+    @Test(priority = 4, description = "Create Customer")
     public void createCustomer() throws IOException {
         customer = new Customer();
         customer.callingCreateCustomerAPI();
     }
-    @Test
+    @Test(priority = 5, description = "Update Customer")
     public void updateCustomer() throws IOException {
         customer = new Customer();
         customer.callingUpdateCustomerAPI(102,"Md Shaon", "shaon@gmail.com","Dhaka","01680235344");
     }
-    @Test
+    @Test(priority = 6, description = "Delete Customer")
     public void deleteCustomer() throws IOException {
         customer = new Customer();
-        customer.callingCustomerDeleteAPI(52327);
+        customer.callingCustomerDeleteAPI(352879);
     }
 }
